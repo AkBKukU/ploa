a7e589838d9feb118a20d36eac9089b4003d1c13
AkBKukU <ShelbyJueden@gmail.com>
2013-01-14 22:59:33 -0700

    General Update

--- index.php
+++ index.php
@@ -1,3 +1,17 @@
-<?php
-	echo "hi";
-?>
+<!DOCTYPE html>
+<html lang="en-US">
+	<head>
+		<title>Ploa Blog Test</title>
+		<?php
+		session_start()
+		?>
+		<meta charset=utf-8>
+		<meta name="description" content="Ploa blog demonstration">
+	</head>
+ 	<body>
+ 		<h1>Blog!</h1>
+<?php include('./blog.php');?>
+
+	
+	</body>
+</html>
