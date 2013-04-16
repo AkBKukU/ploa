497b009c64fc79d345c9e8bd1c49a5cc659d6c73
AkBKukU <shelbyjueden@gmail.com>
2013-04-16 13:37:37 -0700

    v0.72 Help Update 1

--- managment/help.php
+++ managment/help.php
@@ -1,5 +1,15 @@
 
         <h2><p>Help</p></h2>
+        <ul class="controls">
+            <li><a href="#textstyle"><code>STYLE</code></a></li>
+            <li><a href="#textlinks"><code>LINK</code></a></li>
+            <li><a href="#textlists"><code>LIST</code> & <code>NUMLIST</code></a></li>
+            <li><a href="#textimage"><code>IMAGE</code></a></li>
+            <li><a href="#textplace"><code>PLACE</code></a></li>
+            <li><a href="#textsize"><code>SIZE</code></a></li>
+            <li><a href="#textcolor"><code>COLOR</code></a></li>
+            <li><a href="#textwidth"><code>WIDTH</code></a></li>
+        </ul>
         <div id="helppage" class="textarea">
             <p>This page will show you how to add various type of formatting and objects to your posts. Before covering the different formmating tags let's cover how to add formatting to some text. To declare a section of text for formatting you put it in braces and add the appropriate tags like this:</p>
             
@@ -27,10 +37,10 @@
                         
             <p>Here are the avaible tags:</p>
             <ul>
-                <li>U: <u>Underlines the text</u></li>
-                <li>B: <strong>Make the text bold</strong></li>
-                <li>S: <strike>Strikes trough the text</strike></li>
-                <li>I: <em>Italisizes the text</em></li>
+                <li><code>U</code>: <u>Underlines the text</u></li>
+                <li><code>B</code>: <strong>Make the text bold</strong></li>
+                <li><code>S</code>: <strike>Strikes trough the text</strike></li>
+                <li><code>I</code>: <em>Italisizes the text</em></li>
             </ul>
             
             
@@ -58,9 +68,9 @@
             </table>
             
             <p>Writing this out in the writer would look like this:</p>
-            <p>[list;First line</p>
+            <p><code>[list;</code>First line</p>
             <p>Second line</p>
-            <p>Third line]</p>
+            <p>Third line<code>]</code></p>
             
             <hr />
             <h3 id="textlinks"><a href="#textlinks">Links</a></h3>
@@ -125,6 +135,80 @@
             <p>The result of that would look like this:</p><img src="http://upload.wikimedia.org/wikipedia/commons/thumb/4/40/Intertec_Superbrain.jpg/287px-Intertec_Superbrain.jpg" alt="An old computer" title="Intertec Superbrain">
             <p>The alt text is what you see if the image doesn't load like this: <img scr="no-image" alt="Image would go here" title="Missing image example.">. It should be a breif description of the image. You may or may not think that the alt text is optional but it is actually invalid HTML to not have is. This is because people who are visualy impaired or have some other form of obstruction that doesn't allow them to see the image need to be able to know what it is. When using this blog tool you must provide an alt text description.</p>
              
+            <hr />
+            <h3 id="textplace"><a href="#textplace">Placement</a></h3>
+            <p>With these you can add styling such as being bold to your text when you post.</p>
+                        
+            <p>Here are the avaible tags:</p>
+            <ul>
+                <li>U: <u>Underlines the text</u></li>
+                <li>B: <strong>Make the text bold</strong></li>
+                <li>S: <strike>Strikes trough the text</strike></li>
+                <li>I: <em>Italisizes the text</em></li>
+            </ul>
+            
+            
+            <hr />
+            <h3 id="textsize"><a href="#textsize">Size</a></h3>
+            <p>With these you can the size of the text. This code takes more than one value. Here is the proper format:</p>
+            <table>
+                <tr>
+                   <th>Open Brace</th>
+                   <th>Size Tag</th>
+                   <th>Semi-Colon</th>
+                   <th>The Size</th>
+                   <th>Semi-Colon</th>
+                   <th>Text to show</th>
+                   <th>Close Brace</th>
+                </tr>
+                
+                <tr>
+                    <td>[</td>
+                    <td>size</td>
+                    <td>;</td>
+                    <td>big</td>
+                    <td>;</td>
+                    <td>some big text</td>
+                    <td>]</td>
+                </tr>
+            </table>  
+            
+            <p>There are five sizes:</p>  
+            <ul>
+                <li><code>Huge</code>: 4X</li>
+                <li><code>Big</code>: 2X</li>
+                <li><code>Normal</code>: 1X</li>
+                <li><code>Small</code>: 0.5X</li>
+                <li><code>Tiny</code>: 0.25X</li>
+            </ul>
+            
+            
+            <hr />
+            <h3 id="textcolor"><a href="#textcolor">Color</a></h3>
+            <p>With these you can add some color to your post.</p>
+                        
+            <p>Here are the avaible tags:</p>
+            <ul>
+                <li>U: <u>Underlines the text</u></li>
+                <li>B: <strong>Make the text bold</strong></li>
+                <li>S: <strike>Strikes trough the text</strike></li>
+                <li>I: <em>Italisizes the text</em></li>
+            </ul>
+            
+            
+            <hr />
+            <h3 id="textwidth"><a href="#textwidth">Width</a></h3>
+            <p>With these you can add styling such as being bold to your text when you post.</p>
+                        
+            <p>Here are the avaible tags:</p>
+            <ul>
+                <li>U: <u>Underlines the text</u></li>
+                <li>B: <strong>Make the text bold</strong></li>
+                <li>S: <strike>Strikes trough the text</strike></li>
+                <li>I: <em>Italisizes the text</em></li>
+            </ul>
+            
+            
             <hr />
             <h3 id="contact"><a href="#contact">Contact</a></h3>
             <p>If you have any questions, comment, concerns, idea, bug reports, etc please email the developer at <a href="mailto:shelbyjueden@gmail.com">shelbyjueden@gmail.com</a> and I will try to get back to you as quickly as possible.</p>
