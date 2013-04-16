46a1959c7da0268b77f82444da101c6a86e74ef0
AkBKukU <shelbyjueden@gmail.com>
2013-04-15 18:32:10 -0700

    v0.71_1 Icons

---
+++ managment/about.php
@@ -0,0 +1,7 @@
+
+        <h2><p>About</p></h2>
+        <div id="aboutpage" class="textarea">
+            <img class="centered" alt="Setting Icon" src="content/images/ploa logo.png">
+            <p>ploa is meant to be a simple blog platform that can be easily added to an existing website. Since you are reading this it looks like that worked! ploa started as a means to get away from converting an RSS feed of Blogger for having a blog on my webpage. As I started working on it I wondered if there were many other things like what I wanted availble. Turns out there isn't that big of a market for embbedable blogs because I couldn't find very many. So I decided I would release ploa as I contiued working on it. This is my first project that I have made publicly availible and it is exxciting!</p>
+            <p> ploa is developed by <a href="http://www.akbkuku.org:8228/">Shelby Jueden</a></p>
+        </div>
